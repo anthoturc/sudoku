d27e1a82c141bd0ca5abd12d25b0d15dd5cd6ce8
antho <antho.turc@gmail.com>
2019-12-20 00:09:48 -0800

    got rid of unused variables in main file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 
-    private static final String boardsPath = "C:\\Users\\antho\\Desktop\\Side_Projects\\Sudoku\\boards\\";
-    private static final String board = "board1.txt";
-
     public static void main(String[] args) {
         int WIDTH = 700;
         int HEIGHT = 700;
